6a9ac06b92f01ce66e1b4fe6dc22fa6b7165fdfd
Issac <issac.isc.isc@gmail.com>
2025-02-13 18:39:54 -0600

    Update projects section and update certificate links

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -11,14 +11,14 @@ export const certificates: Certificate[] = [
     issuer: "Coursera",
     date: "January 2025",
     credentials:
-      "https://www.coursera.org/account/accomplishments/verify/MAOMZG8E1B49",
+      "https://coursera.org/share/d0399e894ae476a5989ff186cafdf950",
   },
   {
     name: "Google AI Essentials",
     issuer: "Coursera",
     date: "November 2024",
     credentials:
-      "https://www.coursera.org/account/accomplishments/verify/80PEURE2MTP5",
+      "https://coursera.org/share/947278ff2d205d5437795493f863faf7",
   },
   {
     name: "SQL and Relational Database 101",
@@ -32,7 +32,7 @@ export const certificates: Certificate[] = [
     issuer: "Coursera",
     date: "February 2025",
     credentials:
-      "https://courses.cognitiveclass.ai/certificates/ac34a388d2bd46a0826ee10ef636a129",
+      "https://coursera.org/share/6c445d0a9bc03ca6c510833db3838147",
   },
   {
     name: "Getting Started with Git and GitHub",
